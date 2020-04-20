17e463c33f09aab429a59486c6c557171e3dcc5a
sagarsalyan <sagarslyn@gmail.com>
2020-04-20 14:23:18 +0530

    Added DrawerNavigation

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { useIsDrawerOpen, createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  const isDrawerOpen = useIsDrawerOpen();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style = {{fontSize:40}}>{isDrawerOpen}</Text>
+      <Text style = {{fontSize:40}}>Home</Text>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+      <Button onPress={() => navigation.openDrawer()} title="OpenDrawer" />
+      <Button onPress={() => navigation.closeDrawer()} title="CloseDrawer" />
+      <Button onPress={() => navigation.toggleDrawer()} title="ToggleDrawer" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text style = {{fontSize:40}}>Notification</Text>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Text>Sagar</Text>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+> npm install @react-navigation/drawer
+> npm install @react-navigation/native
+> npm install react-native-safe-area-context
+> npm install react-native-screens
+> npm install react-native-gesture-handler
+> npm install react-native-reanimated
